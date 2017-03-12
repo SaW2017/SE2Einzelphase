142138123bdec3f226af0ea8ba56a8aa203b067b
SaW2017 <littlemiss0085@yahoo.de>
2017-03-12 18:57:09 +0100

    Vorläufige Version der App

--- app/src/main/java/com/example/hp/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/hp/se2einzelphase/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.hp.se2einzelphase;
 
+import android.app.AlertDialog;
+import android.support.annotation.FloatRange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.app.AlertDialog;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnCalc;                     //Button zum Durchführen der Division
+    private EditText edDivisor, edDividend;     //Eingabefelder
+    private TextView edOutput;                  //Ausgabewert
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Methode um Werte zu initialisieren wird bei Start der App aufgerufen
+        initialisiere();
+    }
+
+    //Objekte erstellen mit dem jeweiligen Verweis auf das zugehörige Textfeld
+    private void initialisiere(){
+        btnCalc = (Button)findViewById(R.id.btnCalc);
+        edDivisor = (EditText)findViewById(R.id.edDivisor);
+        edDividend = (EditText)findViewById(R.id.edDividend);
+        edOutput = (TextView)findViewById(R.id.edOutput);
+
+        btnCalc.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //Inhalt (Objekt) des jeweiligen Textfeldes abrufen und in Stringvariable konvertieren
+        String divisor = edDivisor.getText().toString();
+        String dividend = edDividend.getText().toString();
+
+        //if (v.getId() == R.id.btnCalc)
+       try {
+           //Bedingung dass Divisor !=0 ist
+           if (!divisor.equals("0")) {
+
+               //String in Float umwandeln
+               float flDivisor = Float.parseFloat(divisor);
+               float flDividend = Float.parseFloat(dividend);
+
+               //Berechung durchführen und in float ergebnis speichern
+               float ergebnis = (flDividend/flDivisor) ;
+
+               //Ergebnis in Feld Output speichern und vorher in Stringvariable konvertieren
+               edOutput.setText(Float.toString(ergebnis));
+
+               //Fehlermeldung bei Division durch 0
+           } else {
+               //Fehlerdialog erstellen
+               AlertDialog.Builder error = new AlertDialog.Builder(this);
+               error.setTitle("Fehler!");
+               error.setMessage("Durch 0 dividieren ist nicht möglich!");
+               error.setCancelable(true);
+
+               //Fehlerdialog anzeigen
+               AlertDialog errorDialog = error.create();
+               errorDialog.show();
+           }
+       }catch(Exception e){
+           edOutput.setText("Division nicht möglich!");
+       }
     }
 }
